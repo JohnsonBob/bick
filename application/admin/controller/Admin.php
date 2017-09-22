2e3b2f4fe1e91930d11c99c2246bf56a5a866da6
Johnson <poiuytrengo@qq.com>
2017-09-22 17:37:19 +0800

    静态模板添加

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -4,6 +4,9 @@ namespace app\admin\controller;
 
 class Admin extends Base
 {
+   /* public function _initialize(){
+        $admin = model('admin');
+    }*/
     public function lst()
     {
         return $this->fetch();
@@ -11,6 +14,21 @@ class Admin extends Base
 
     public function add()
     {
+        if($_POST){
+            //var_dump($_POST);
+            $admin = model('admin');
+            $res = $admin->save(input('post.'));
+           // $res = db('admin')->insert(input('post.'));
+            echo $res;
+            die();
+            if($res){
+                $this->success('管理员添加成功',url('lst'));
+            }else{
+
+                $this->error('管理员添加失败',url('add'));
+            }
+            return;
+        }
         return $this->fetch();
     }
 
