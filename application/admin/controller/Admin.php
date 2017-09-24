9e7cd127b4109aad336dbf7271850d951ac7ea96
Johnson <poiuytrengo@qq.com>
2017-09-24 20:48:44 +0800

    添加管理员 修改管理员完成

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -7,8 +7,15 @@ class Admin extends Base
    /* public function _initialize(){
         $admin = model('admin');
     }*/
+
     public function lst()
     {
+        $admin = model('admin');
+        $res = $admin->getAdmin();
+//        dump($res) ;
+//        die();
+        $this->assign('adminres',$res);
+
         return $this->fetch();
     }
 
@@ -17,11 +24,7 @@ class Admin extends Base
         if($_POST){
             //var_dump($_POST);
             $admin = model('admin');
-            $res = $admin->save(input('post.'));
-           // $res = db('admin')->insert(input('post.'));
-            echo $res;
-            die();
-            if($res){
+            if($admin->addAdmin($_POST)){
                 $this->success('管理员添加成功',url('lst'));
             }else{
 
@@ -32,8 +35,20 @@ class Admin extends Base
         return $this->fetch();
     }
 
-    public function edit()
+    public function edit($id)
     {
+        if($_POST){
+            $admin = model('admin');
+            if($admin->editAdmin(input('post.'))){
+                $this->success('管理员密码修改成功',url('lst'));
+            }else{
+
+                $this->error('管理员密码修改失败',url('edit'));
+            }
+            return;
+        }
+        $admin = db('admin')->find($id);
+        $this->assign('admin',$admin);
         return $this->fetch();
     }
 
--- application/admin/model/Admin.php
+++ application/admin/model/Admin.php
@@ -13,4 +13,50 @@ class Admin extends Model
         //TODO:自定义的初始化
     }
 
+    /**
+     *  获取所有管理员并分页展示
+     */
+    public  function getAdmin(){
+        return $this->paginate(5);
+    }
+
+    /**
+     *  添加管理员到数据库
+     */
+    public  function addAdmin($data){
+        if(empty($data) || !is_array($data)){
+            return false;
+        }
+        if($data['password']){
+            $data['password'] = md5($data['password']);
+            if($this->save($data)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 修改管理员信息
+     * @param $date
+     */
+    public function editAdmin($data){
+        if(empty($data) || !is_array($data)){
+            return false;
+        }
+
+        if($data['password']){
+            $data['password'] = md5($data['password']);
+            $res = $this->save([
+                'name'  => $data['name'],
+                'password' => $data['password']
+            ],['id' => $data['id']]);
+            if($res){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
